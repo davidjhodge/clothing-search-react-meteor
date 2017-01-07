bee9f6df9a0b7a569b9160cbc0c8e10e8635503a
davidjhodge <dhodge@genesisappsonline.com>
2017-01-06 21:19:18 -0500

    Implement Search Bar

--- imports/ui/Product.jsx
+++ imports/ui/Product.jsx
@@ -2,27 +2,71 @@ import React, { Component, PropTypes } from 'react';
 
 const styles = {
   card: {
+    display: 'flex',
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
+    textAlign: 'center',
+    color: 'black',
+    paddingHorizontal: 24,
+    paddingBottom: 16,
+    margin: 8,
+    borderRadius: 4,
+    cursor: 'pointer',
+    float: 'left',
+    height: 322,
+    width: 220 - 32,
   },
   image: {
     height: 194,
     maxWidth: 172,
     resizeMode: 'contain',
     alignSelf: 'center',
+    paddingBottom: 8,
+  },
+  title: {
+    fontSize: 14,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    overflow: 'hidden',
+    height: 33,
+  },
+  price: {
+    fontSize: 14,
+    paddingTop: 8,
+    height: 20
   }
-}
+};
+
+// background: #ffffff;
+// text-align: center;
+// color: #000000;
+// /*display: inline-block;*/
+// text-align: center;
+// /*width: calc(25% - 2*32px);*/
+// height: 322px;
+// /*width: 220px;*/
+// padding:16px;
+// margin: 16px;
+// border-radius: 4px;
+// box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+// transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+// cursor: pointer;
+// float: left;
 
 class Product extends Component {
   render() {
     return (
-      <div style={styles.card}>
-        <img src={this.props.imageUrl} style={styles.image}/>
-        <span style={styles.title}>{this.props.title}</span>
-        <span style={styles.price}>{this.props.price}</span>
-      </div>
+      <a href={this.props.outboundUrl} target="_blank">
+        <div style={styles.card}>
+          <img src={this.props.imageUrl} style={styles.image}/>
+          <span style={styles.title}>{this.props.title}</span>
+          <span style={styles.price}>{this.props.price}</span>
+        </div>
+      </a>
+
     );
   }
 }
--- imports/ui/ProductSearch.jsx
+++ imports/ui/ProductSearch.jsx
@@ -1,63 +1,97 @@
 import React, { Component, PropTypes } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import Responsive  from 'react-grid-layout';
-import '../../node_modules/react-grid-layout/css/styles.css';
-import '../../node_modules/react-resizable/css/styles.css';
-
 import { GridList, GridTile } from 'material-ui/GridList';
 import Product from './Product.jsx';
 
 const styles = {
-  grid: {
-    backgroundColor: '#f5f5f5',
-    padding: 16,
-    display: 'inline-block',
+  pageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
   },
+  grid: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: 'transparent',
+    padding: 16,
+    flex: 1,
+    height: 'auto',
+    alignSelf: 'center',
+    maxWidth: 1024,
+  },
+  searchBar: {
+    display: 'flex',
+    flex: 1,
+    borderRadius: 4,
+    borderColor: 'transparent',
+    outline: 0,
+    height: 48,
+    fontSize: 14,
+    color: 'black',
+    paddingLeft: 16,
+    maxWidth: 1024,
+  },
+  searchContainer: {
+    alignSelf: 'stretch',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 16,
+  }
 };
-//
-// background: #f5f5f5;
-// padding: 16px;
-// /*overflow-y: auto;*/
-// display: inline-block;
-// margin: auto;
 
 class ProductSearch extends Component {
   constructor(props) {
     super(props);
     this.state =  {
-      products: []
+      searchString: "",
+      products: [],
     };
   }
-  aggregateSearch() {
-    Meteor.call('aggregateSearch', 'mens clothing', (error,response) => {
-      if (error) {
-        console.log(error);
-      } else {
-        // response is an array of simple products
-        this.setState({
-          products: response
-        });
-      }
-    });
+  aggregateSearch(event) {
+    // Cancels page refresh from form submission
+    event.preventDefault();
+
+    if (this.state.searchString.length > 0) {
+      Meteor.call('aggregateSearch', this.state.searchString, (error,response) => {
+        if (error) {
+          console.log(error);
+        } else {
+          // response is an array of simple products
+          this.setState({
+            products: response
+          });
+        }
+      });
+    }
+  }
+
+  searchTextChanged(event) {
+    searchText = event.target.value;
+    this.state.searchString = searchText;
   }
 
   render() {
-    var layout = [{i: 'default', x: 0, y: 0, w:2, h:8, static:true}];
     return (
-      <div>
-        <RaisedButton
-          label="Aggregate Search"
-          style={{margin: 16}}
-          onTouchTap={this.aggregateSearch.bind(this)} />
-        <Responsive
-          className="layout"
-          style={styles.grid}
-          layout={layout}
-          cols={8}
-          rowHeight={30}
-          width={600}>
-          {this.state.products.map((product) => (
+      <div style={styles.pageContainer}>
+        <form
+          onSubmit={this.aggregateSearch.bind(this)}
+          style={styles.searchContainer}>
+          <input
+            type="text"
+            placeholder="Search 1000s of brands..."
+            style={styles.searchBar}
+            onChange={this.searchTextChanged.bind(this)} />
+        </form>
+        <div
+          style={styles.grid}>
+        {this.state.products.map((product) => (
             <Product
               key={product.id}
               imageUrl={product.imageUrl}
@@ -65,7 +99,7 @@ class ProductSearch extends Component {
               price={product.price}
               outboundUrl={product.outboundUrl} />
           ))}
-        </Responsive>
+        </div>
       </div>
     );
   }
