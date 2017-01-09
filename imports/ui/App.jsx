eeb521e8b805ce47c0cb642ac8fc9baa57a9e96d
davidjhodge <dhodge@genesisappsonline.com>
2017-01-08 19:36:44 -0500

    Add normal CSS styling to some components
    A few of the components are now using normal css,
    because inline styles have limitations including limited to no
    support for media queries and responsive design.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,30 +2,17 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 
 import Navigation from './Navigation.jsx';
-import Footer from './Footer.jsx';
-
-const styles = {
-  horizontalContainer: {
-    flex: 1,
-    alignItems: 'stretch',
-    background: 'transparent',
-    flexDirection: 'row',
-  },
-  verticalContainer: {
-    flexDirection: 'column',
-    flex: 1,
-    background: '#f5f5f5',
-  },
-}
+import Footer from './Footer/Footer.jsx';
+import './App.css';
 
 // Instead of a React Component, a stateless function is used for simplicity
 const App = ( {children}) => (
-  <div style={styles.horizontalContainer}>
-    <div style={styles.verticalContainer}>
+  <div className="parentContainer">
       <Navigation />
-      { children }
+      <div className="contentsContainer">
+        { children }
+      </div>
       <Footer />
-    </div>
   </div>
 )
 
---
+++ imports/ui/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+// import styles
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return(
+      <div className="footerContainer">
+        <div className="seperator" />
+        <ul className="list">
+          <li>
+            <Link
+              to="/our-mission"
+              activeClassName="active"
+              className="link">Our Mission</Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              activeClassName="active"
+              className="link">Contact</Link>
+          </li>
+        </ul>
+        <span className="footerText">Copyright Layers, 2016</span>
+      </div>
+    );
+  }
+}
+
+export default Footer;
