84f3b8f1cfecb3889524f65ed42c572dedbdb34f
davidjhodge <dhodge@genesisappsonline.com>
2017-01-17 22:33:17 -0500

    Improvements to Entire App
    Contact Form, Footer, Vision, and Product Page are updated.

--- imports/ui/Product/Product.jsx
+++ imports/ui/Product/Product.jsx
@@ -3,33 +3,33 @@ import './Product.css';
 
 const styles = {
   card: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    textAlign: 'center',
-    color: 'black',
-    borderRadius: 4,
-    cursor: 'pointer',
-    float: 'left',
+    // display: 'flex',
+    // flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: 'white',
+    // textAlign: 'center',
+    // color: 'black',
+    // borderRadius: 4,
+    // cursor: 'pointer',
+    // float: 'left',
   },
   productContents: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // display: 'flex',
+    // flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   imageContainer: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'row',
-    maxHeight: 194,
-    paddingBottom: 8,
-    justifyContent: 'center',
-    backgroundSize: 'contain',
+    // display: 'flex',
+    // flex: 1,
+    // flexDirection: 'row',
+    // maxHeight: 194,
+    // paddingBottom: 8,
+    // justifyContent: 'center',
+    // backgroundSize: 'contain',
   },
   image: {
     // resizeMode: 'contain',
@@ -41,9 +41,9 @@ class Product extends Component {
   render() {
     return (
       <a href={this.props.outboundUrl} target="_blank">
-        <div style={styles.card} className="card">
-          <div style={styles.productContents} className="productContents">
-            <div style={styles.imageContainer} className="image-container">
+        <div className="card">
+          <div className="productContents">
+            <div className="img-container">
               <img src={this.props.imageUrl} style={styles.image} className="image"/>
             </div>
             <span className="title">{this.props.title}</span>
