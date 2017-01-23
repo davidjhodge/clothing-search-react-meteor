3d4022026a8fbd56f6aa5bf9f43d80ad3a87d4d7
davidjhodge <dhodge@genesisappsonline.com>
2017-01-22 19:27:11 -0500

    Add User Accounts Functionality and more Favicons
    This commit contains the accounts-ui package, which support user
    login with email and password as well as Facebook. Additional styling and integrating with MongoDB still needs to be done.
    
    Also, favicons for all devices are now ncluded.

---
+++ imports/ui/Accounts/AccountsUIWrapper.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import Blaze from 'meteor/gadicc:blaze-react-component';
+
+export default class AccountsUIWrapper extends Component {
+  render() {
+    return (
+      <Blaze template="loginButtons" align="right" />
+    );
+  }
+}
